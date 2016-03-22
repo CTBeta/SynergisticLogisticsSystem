67bc60c2e3ce7a2f19006721083439331c00bc53
Jason C.T <ctrysbita@outlook.com>
2016-03-22 17:43:21 +0800

    加入串口通信

--- LandMain/LandMain/LandMain.ino
+++ LandMain/LandMain/LandMain.ino
@@ -1,42 +1,23 @@
 #include <SPI.h>
-#include <RFID.h>
 #include <CTB_SIM900A.h>
 #include <CTB_Stepper.h>
-#include <Wire.h>
 #include <PWMServoDriver.h>
 int d[4]={0,0,0,0};//已占用货柜号
 CTB_Stepper SM;
 PWMServoDriver pwm = PWMServoDriver();
 const int SMPinY=9,SMPinZ=7,PinY_CW=8,PinZ_CW=6,SPEED=100; //步进电机参数
-RFID rfid(10,5); //D10--读卡器MOSI引脚、D5--读卡器RST引脚
 //设置旋转角度
 #define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  450 // this is the 'maximum' pulse length count (out of 4096)
-uint8_t servonum ;
-//RC522
-//4字节卡序列号，第5字节为校验字节
-unsigned char serNum[5];
-//写卡数据
-unsigned char sectorKeyA[16][16] = {
- {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
- {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
- {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},};
-//新扇区A密码，16个扇区，每个扇区密码6Byte
-unsigned char sectorNewKeyA[16][16] = {
- {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
- {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xff,0x07,0x80,0x69, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
- {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xff,0x07,0x80,0x69, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},};
 
 void setup()
 {
    Serial.begin(115200);
    SPI.begin();
    pwm.begin();
-   rfid.init();
    SM.init(SMPinY,SMPinZ,PinY_CW,PinZ_CW,SPEED);
    Serial.println("16 channel Servo test!");
    pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
-   Wire.begin();
 }
 void setServoPulse(uint8_t n, double pulse) {
   double pulselength;
@@ -57,129 +38,86 @@ void loop()
    char sms[50]={
     'Y','o','u',' ','h','a','v','e',' ','e','x','p','r','e','s','s','a','g','e','(','s',')',','
     ,'a','n','d',' ','t','h','e',' ','p','a','s','s','w','o','r','d',' ','i','s',':','*','*','*','*'};
-   char c[4];//储存临时随机密码
-
-   int x,y=0;//y：货柜号   st：载物台移动步数
+   int c[4];//储存临时随机密码
+   int x,y,i=0;//y：货柜号   st：载物台移动步数
    int st1,st2=5000;
+   int servonum;
    CTB_SIM900A SIM;
-   unsigned char i,tmp;
-   unsigned char status;
-   unsigned char str[MAX_LEN];
-   unsigned char RC_size;
-   unsigned char blockAddr; //选择操作的块地址0～63
-   //找卡
-   rfid.isCard();
-   //读取卡序列号
-   if (rfid.readCardSerial())
+   i=Serial.read();
+   Serial.println(i);
+   delay(1000);
+   if(i==1)
    {
-  
-     Serial.print(rfid.serNum[0],HEX);
-     Serial.print(rfid.serNum[1],HEX);
-     Serial.print(rfid.serNum[2],HEX);
-     Serial.print(rfid.serNum[3],HEX);
-     Serial.print(rfid.serNum[4],HEX);
-     Serial.println(" ");
-   }
-  
-   //选卡，返回卡容量（锁定卡片，防止多次读写）
-   rfid.selectTag(rfid.serNum);
-   
-   //读卡
-   blockAddr = 7; //数据块7
-   status = rfid.auth(PICC_AUTHENT1A, blockAddr, sectorNewKeyA[blockAddr/4], rfid.serNum);
-   if (status == MI_OK) //认证
-   {
-     //读数据
-     blockAddr = blockAddr - 3 ; //数据块4
-     if( rfid.read(blockAddr, str) == MI_OK)
+     //发送短信及取货密码
+     SIM.init();//初始化SIM900A
+     for(x=0;x<=10;x++)
      {
-       //输出卡内内容
-       Serial.print("Read from the card ,the data is : ");
-       Serial.println((char *)str);
-       //发送短信及取货密码
-       SIM.init();//初始化SIM900A
-       for(x=0;x<=10;x++)
+      tel[x]=Serial.read();//提取收件人电话号码
+     }
+     for(x=43;x<=46;x++)
+     {
+       sms[x]=random(1,8)+48;//产生随机密码
+     }
+     //暂存密码
+     c[0]=sms[43];
+     c[1]=sms[44];
+     c[2]=sms[45];
+     c[3]=sms[46];
+     SIM.SendSMS(tel,sms);//发送短信
+     y=random(1,4);//产生货柜号
+     //存储货柜号
+     for(x=0;x<=3;x++)
+     {
+       if(y==d[x])
        {
-        tel[x]=str[x];//提取收件人电话号码
+         y=random(1,4);
        }
-       for(x=43;x<=46;x++)
-       {
-        sms[x]=random(1,8)+48;//产生随机密码
-       }
-       //暂存密码
-       c[0]=sms[43];
-       c[1]=sms[44];
-       c[2]=sms[45];
-       c[3]=sms[46];
-       SIM.SendSMS(tel,sms);//发送短信
-       for(x=0;x<=3;x++)
-       {
-       Wire.beginTransmission(1);
-       Wire.write("233");
-       Wire.write(c);
-       Wire.endTransmission();
-       }
-       Wire.beginTransmission(1);
-       Wire.write("233");
-       Wire.write(c);
-       Wire.endTransmission();
-       y=random(1,4);//产生货柜号
-       //存储货柜号
-       for(x=0;x<=3;x++)
-       {
-         if(y==d[x])
-         {
-           y=random(1,4);
-         }
-       }y=1;
-      
-        for(x=0;x<=3;x++)
-       {
-         Serial.println(d[x]);
-       }
-       servonum=y-1;
-       //取载物台移动步数
-       if(y<=2)
-       {
-         st1=5000;
-         st2=y*5000;
-       }
-       else
-       {
-         st1=5000;
-         st2=y/2*5000;
-       }
-       //移动载物台至指定货柜
-       for(x=0;x<=st1;x++)
-       {
-         SM.StepY(0);
-       }
-       for(x=0;x<=st2;x++)
-       {
-         SM.StepZ(0);
-       }
-       // Drive each servo one at a time
-       Serial.print("servonum:");
-       Serial.println(servonum);
-       for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-         pwm.setPWM(0, 0, pulselen);
-         pwm.setPWM(1, 0, pulselen);
-       }
-       delay(3000);
-       for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-         pwm.setPWM(0, 0, pulselen);
-         pwm.setPWM(1, 0, pulselen);
-       }
-       //载物台复位
-       for(x=0;x<=st1;x++)
-       {
-         SM.StepY(1);
-       }
-       for(x=0;x<=st2;x++)
-       {
-         SM.StepZ(1);
-       }
+     }y=1;
+     for(x=0;x<=3;x++)
+     {
+       Serial.println(d[x]);
+     }
+     servonum=y-1;
+     //取载物台移动步数
+     if(y<=2)
+     {
+       st1=5000;
+       st2=y*5000;
+     }
+     else
+     {
+       st1=5000;
+       st2=y/2*5000;
+     }
+     //移动载物台至指定货柜
+     for(x=0;x<=st1;x++)
+     {
+       SM.StepY(0);
+     }
+     for(x=0;x<=st2;x++)
+     {
+       SM.StepZ(0);
+     }
+     // Drive each servo one at a time
+     Serial.print("servonum:");
+     Serial.println(servonum);
+     for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+       pwm.setPWM(0, 0, pulselen);
+       pwm.setPWM(1, 0, pulselen);
+     }
+     delay(3000);
+     for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+       pwm.setPWM(0, 0, pulselen);
+       pwm.setPWM(1, 0, pulselen);
+     }
+     //载物台复位
+     for(x=0;x<=st1;x++)
+     {
+       SM.StepY(1);
+     }
+     for(x=0;x<=st2;x++)
+     {
+       SM.StepZ(1);
      }
    }
-   rfid.halt();
 }
--- LandMain/LandMain1/LandMain1.ino
+++ LandMain/LandMain1/LandMain1.ino
@@ -1,26 +1,60 @@
-#include <Wire.h>
-void setup() {
-  Serial.begin(115200);
-  Wire.begin(1);
-  Wire.onReceive(receiveEvent); // register event
+#include <SPI.h>
+#include <RFID.h>
+
+RFID rfid(10,5);    //D10--读卡器MOSI引脚、D5--读卡器RST引脚
+
+//4字节卡序列号，第5字节为校验字节
+unsigned char serNum[5];
+//原扇区A密码，16个扇区，每个扇区密码6Byte
+unsigned char sectorKeyA[16][16] = {
+        {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
+        {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
+        {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},};
+//新扇区A密码，16个扇区，每个扇区密码6Byte
+unsigned char sectorNewKeyA[16][16] = {
+        {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
+        {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xff,0x07,0x80,0x69, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
+        {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xff,0x07,0x80,0x69, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},};
+
+void setup()
+{
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.init();
 }
 
-void loop() {
-  delay(100);
-}
+void loop()
+{
+  int x=0;
+  unsigned char i,tmp;
+  unsigned char status;
+  unsigned char str[MAX_LEN];
+  unsigned char RC_size;
+  unsigned char blockAddr;        //选择操作的块地址0～63
 
-void receiveEvent(int howMany) {
-  char a[4];
-  Wire.requestFrom(1, 4);    // request 6 bytes from slave device #2
-
-  while (Wire.available())   // slave may send less than requested
+  //找卡
+  rfid.isCard();
+  if (rfid.readCardSerial())
   {
-    for(int x=0;x<=3;x++)
-    {
-      a[x] = Wire.read(); // receive a byte as character
-    }
-    Serial.print(a);         // print the character
+   blockAddr = 7;
   }
+  //选卡，返回卡容量（锁定卡片，防止多次读写）
+  rfid.selectTag(rfid.serNum);
 
-  delay(500);
+  //读卡
+  blockAddr = 7;                //数据块7
+  status = rfid.auth(PICC_AUTHENT1A, blockAddr, sectorNewKeyA[blockAddr/4], rfid.serNum);
+  if (status == MI_OK)  //认证
+  {
+    //读数据
+    blockAddr = blockAddr - 3 ; //数据块4
+    if( rfid.read(blockAddr, str) == MI_OK)
+    {
+      for(x=0;x<=10;x++)
+      {
+        Serial.print(str[x]-48);
+      }
+    }
+  }
+  rfid.halt();
 }
