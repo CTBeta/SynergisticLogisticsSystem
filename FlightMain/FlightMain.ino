a057a65f508b67ea0945248aecc2962e0bcb1666
Jason C.T <ctrysbita@outlook.com>
2016-03-23 10:38:04 +0800

    FlightMain update

--- FlightMain/FlightMain.ino
+++ FlightMain/FlightMain.ino
@@ -2,9 +2,14 @@ int tmp;
 void setup() {
   pinMode(2,INPUT);
   pinMode(3,OUTPUT);
+  pinMode(13,OUTPUT);
 }
 
 void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);   
   if(digitalRead(2)==1&&tmp!=1)
   {
     digitalWrite(3,HIGH);
