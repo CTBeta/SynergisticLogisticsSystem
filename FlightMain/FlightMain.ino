163a9b8405e38d8d1a31b08a2e12e7806ce43e9f
Jason C.T <ctrysbita@outlook.com>
2016-03-26 14:09:09 +0800

    FlightMain Update

--- FlightMain/FlightMain.ino
+++ FlightMain/FlightMain.ino
@@ -1,8 +1,12 @@
-int tmp;
+#include <Servo.h> 
+Servo myservo;
+int tmp=0;
 void setup() {
-  pinMode(2,INPUT);
-  pinMode(3,OUTPUT);
+  pinMode(8,INPUT);
+  myservo.attach(9);
   pinMode(13,OUTPUT);
+  myservo.write(0);
+  
 }
 
 void loop() {
@@ -10,10 +14,10 @@ void loop() {
   delay(1000);              // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);   
-  if(digitalRead(2)==1&&tmp!=1)
+  if(digitalRead(8)==1&&tmp!=1)
   {
-    digitalWrite(3,HIGH);
     delay(5000);
+    myservo.write(180); 
     tmp=1;
   } 
 }
