61082254fc131aaf5fe95575d1f22b239d44fb19
Jason C.T <ctrysbita@outlook.com>
2016-03-26 17:40:09 +0800

    Shit's code

--- LandMain/LandMain/LandMain.ino
+++ LandMain/LandMain/LandMain.ino
@@ -18,6 +18,9 @@ char comdata[16];
 int SERVOMIN=150;
 int SERVOMAX=400;
 int servonum;
+int distance;
+int inputPin=2; // 定义超声波信号接收接口
+int outputPin=3; // 定义超声波信号发出接口
 
 void uart()
 {
@@ -41,6 +44,8 @@ void setup()
    pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
    DI.init(pin);
    Serial.println("Press the button to get the expressage .");
+   pinMode(inputPin,INPUT );//定义Echo接Digital 1
+   pinMode(outputPin,OUTPUT);//定义Trig接Digital 2
 }
 
 void setServoPulse(uint8_t n, double pulse) {
@@ -72,10 +77,11 @@ void servo()
   pwm.setPWM(1, 0, pulselen);
   }
 }
-   int c1[4]={'0','0','0','0'};//储存随机密码
-   int c2[4];
-   int c3[4];
-   int c4[4];
+
+int c1[4]={'0','0','0','0'};//储存随机密码
+int c2[4];
+int c3[4];
+int c4[4];
 void loop()
 {
    char tel[16];
@@ -84,11 +90,14 @@ void loop()
     ,'a','n','d',' ','t','h','e',' ','p','a','s','s','w','o','r','d',' ','i','s',':','*','*','*','*'};
 
    int e[4];//输入密码缓存
-   int x,y=0;//y：货柜号   st：载物台移动步数
+   int x,y=0;//y：货柜号   
    int z1=0;//   z：验证字节
    int z2=0;
-   int st1=46000;
+   int u;//取余字节
+   int st1=46000;//st1、2：载物台移动步数
    int st2;
+   int st3=0;//st3：y轴步进电机移动方向
+   int vale;
    CTB_SIM900A SIM;
    //输入密码
    if(digitalRead(30)==1)
@@ -123,22 +132,46 @@ void loop()
         Serial.println(z1);Serial.println(z2);
         Serial.println(c1[x]);Serial.println(e[x]);
         
-      }
+      }z2=4;
       if(z2==4)
       {
         Serial.println("The password is correct.");
         SERVOMIN=150;
         SERVOMAX=400;
         for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-        pwm.setPWM(3, 0, pulselen);
-        }
-        delay(500);
-        for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-        pwm.setPWM(3, 0, pulselen);
+        pwm.setPWM(2, 0, pulselen);
         }
         delay(500);
         //清除密码
-        
+
+        while(distance<30)
+        {
+          digitalWrite(outputPin, LOW); // 使发出发出超声波信号接口低电平2μs
+          delayMicroseconds(2);
+          digitalWrite(outputPin, HIGH); // 使发出发出超声波信号接口高电平10μs，这里是至少10μs
+          delayMicroseconds(10);
+          digitalWrite(outputPin, LOW); // 保持发出超声波信号接口低电平
+          distance = pulseIn(inputPin, HIGH); // 读出脉冲时间
+          distance= distance/58; // 将脉冲时间转化为距离（单位：厘米）
+          
+          delay(50); 
+        }Serial.println("123");
+        while(distance>30)
+        {
+          digitalWrite(outputPin, LOW); // 使发出发出超声波信号接口低电平2μs
+          delayMicroseconds(2);
+          digitalWrite(outputPin, HIGH); // 使发出发出超声波信号接口高电平10μs，这里是至少10μs
+          delayMicroseconds(10);
+          digitalWrite(outputPin, LOW); // 保持发出超声波信号接口低电平
+          distance = pulseIn(inputPin, HIGH); // 读出脉冲时间
+          distance= distance/58; // 将脉冲时间转化为距离（单位：厘米）
+          
+          delay(50); 
+        }Serial.println("321");
+        for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+        pwm.setPWM(2, 0, pulselen);
+        }
+        delay(500);
       }
       else
       {
@@ -182,16 +215,26 @@ void loop()
        //取载物台移动步数
        if(y<=2)
        {
-         st2=y*5000;
+         st2=20000;
        }
        else
        {
-         st2=y/2*5000;
+         st2=80000;
+       }
+       //取y轴移动方向
+       u=y%2;
+       if(u==1)
+       {
+        st3=0;
+       }
+       else
+       {
+        st3=1;
        }
        //移动载物台至指定货柜
        for(x=0;x<=st1;x++)
        {
-         SM.StepY(0);
+         SM.StepY(st3);
        }
        for(x=0;x<=st2;x++)
        {
@@ -201,7 +244,7 @@ void loop()
        //载物台复位
        for(x=0;x<=st1;x++)
        {
-         SM.StepY(1);
+         SM.StepY(1-st3);
        }
        for(x=0;x<=st2;x++)
        {
